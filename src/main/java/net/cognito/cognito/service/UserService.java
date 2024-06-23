04f28c1b65aabcfd19271b6971f0f719b3d33fa7
Shreeraam G <shreeram0312@gmail.com>
2024-06-23 16:17:59 +0530

    Create user service & repository

---
+++ src/main/java/net/cognito/cognito/service/UserService.java
@@ -0,0 +1,39 @@
+package net.cognito.cognito.service;
+
+import net.cognito.cognito.model.User;
+import net.cognito.cognito.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        // TODO: Handle user not found exception here.
+
+        return user.get();
+    }
+
+    public User getUserById(String id) {
+        Optional<User> user = userRepository.findById(id);
+
+        // TODO: Handle user not found exception here.
+
+        return user.get();
+    }
+}
