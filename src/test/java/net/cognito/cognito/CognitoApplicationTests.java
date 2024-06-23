78e055c2d272b62c465d574c98a1b61aa3c06f38
Shreeraam G <shreeram0312@gmail.com>
2024-06-23 13:01:54 +0530

    Initial Commit

---
+++ src/test/java/net/cognito/cognito/CognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cognito.cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
